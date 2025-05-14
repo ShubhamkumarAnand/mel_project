fbc715b994caa4b36e73217a517d774a2ef44f4a
imskanand <the.anand3099@gmail.com>
2025-05-14 12:29:46 +0530

    feat: Migration func in database - created workout store with create and get function - setup for the db & migrations

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/ShubhamkumarAnand/melkey-go/mel_project/internal/api"
 	"github.com/ShubhamkumarAnand/melkey-go/mel_project/internal/store"
+	"github.com/ShubhamkumarAnand/melkey-go/mel_project/migrations"
 )
 
 type Application struct {
@@ -21,6 +22,11 @@ func NewApplication() (*Application, error) {
 	pgDB, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDB, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
